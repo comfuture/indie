c66fe2543d3758089e3c0e8d99e842ca2438e4dd
comfuture <comfuture@gmail.com>
2018-05-09 18:42:43 +0900

    make jest to recognize es6

--- src/transaction.js
+++ src/transaction.js
@@ -1,10 +1,5 @@
 import Store from './store'
 
-IDBTransaction = function() {
-
-  window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction
-}()
-
 export default class Transaction {
   constructor(db, stores, mode = 'readwrite') {
     this.transaction = db.transaction(stores, mode)
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
-const indie = require('../lib/indie')
+// const indie = require('../lib/indie')
+import indie from '../src/index'
 
 test('indie should returns promise', () => {
   expect(indie('test_db') instanceof Promise)
